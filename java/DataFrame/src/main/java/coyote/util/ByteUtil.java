d6c69f91063b85bf293926e4355f5242a2e78db8
Steve Cote <sdcote@gmail.com>
2014-01-21 20:52:58 -0500

    Added licensng headers.

--- java/DataFrame/src/main/java/coyote/util/ByteUtil.java
+++ java/DataFrame/src/main/java/coyote/util/ByteUtil.java
@@ -1,10 +1,9 @@
 /*
- * DataFrame - a data marshaling toolkit
- * Copyright (C) 2006 Stephan D. Cote' - All rights reserved.
+ * Copyright (c) 2006 Stephan D. Cote' - All rights reserved.
  * 
  * This program and the accompanying materials are made available under the 
- * terms of the Eclipse Public License v1.0  which accompanies this 
- * distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
  *
  * Contributors:
  *   Stephan D. Cote 
