dbd9a848c758dbe7d99f0eeca8cbb17afe15ce1b
Steve Cote <sdcote@gmail.com>
2014-05-04 18:02:41 -0400

    Initial operational version of MapMarshal

--- src/main/java/coyote/dataframe/marshal/MapFrame.java
+++ src/main/java/coyote/dataframe/marshal/MapFrame.java
@@ -3,6 +3,7 @@ package coyote.dataframe.marshal;
 import java.util.HashMap;
 import java.util.Map;
 
+import coyote.dataframe.DataField;
 import coyote.dataframe.DataFrame;
 
 
@@ -18,8 +19,7 @@ public class MapFrame {
    * 
    * @return
    */
-  public DataFrame marshal( Map<?, ?> source )
-  {
+  public DataFrame marshal( Map<?, ?> source ) {
     DataFrame retval = new DataFrame();
 
     if( source != null ) {
@@ -46,17 +46,55 @@ public class MapFrame {
 
 
   /**
+   * Return the given dataframe as a map of values.
    * 
-   * @param frame
+   * <p>The hierarchy is represented as a Map of Maps.</p>
    * 
-   * @return
+   * <p>Only types supported by DataFrame will be placed in the Map.</p>
+   * 
+   * @param frame The frame from which data is to be retrieved
+   * 
+   * @return A map of objects
    */
   @SuppressWarnings("rawtypes")
-  public Map<?, ?> marshal( DataFrame frame )
-  {
-    return new HashMap();
+  public Map<?, ?> marshal( DataFrame frame ) {
+    Map retval = new HashMap();
+    if( frame != null )
+      retval = recurse( frame, retval );
+    return retval;
   }
-  
-  
+
+
+
+
+  /**
+   * Recurse into the a dataframe, building a Map as it goes.
+   * 
+   * @param frame The frame being recursed into, providing data for the Map 
+   * @param map The Map into which values are placed.
+   */
+  @SuppressWarnings({ "rawtypes", "unchecked" })
+  private static Map recurse( DataFrame frame, Map<?, ?> map ) {
+    Map retval = new HashMap();
+    if( frame != null ) {
+
+      for( int x = 0; x < frame.getFieldCount(); x++ ) {
+        final DataField field = frame.getField( x );
+        String fname = field.getName();
+
+        if( fname == null )
+          fname = "field" + x;
+
+        if( field.isFrame() )
+          retval.put( fname, recurse( (DataFrame)field.getObjectValue(), map ));
+        else
+          retval.put( fname, field.getObjectValue() );
+
+      } // for each field
+
+    } // frame !null
+
+    return retval;
+  }
   
 }
--- src/test/java/coyote/dataframe/marshal/MapFrameTest.java
+++ src/test/java/coyote/dataframe/marshal/MapFrameTest.java
@@ -4,10 +4,12 @@
 package coyote.dataframe.marshal;
 
 //import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -25,10 +27,9 @@ public class MapFrameTest {
    */
   @Test
   @SuppressWarnings({ "rawtypes" })
-  public void testMarshalMap()
-  {
+  public void testMarshalMap() {
     HashMap data = new HashMap();
-    data.put( "One", "One" );  
+    data.put( "One", "One" );
     data.put( "Two", 2 );
 
     MapFrame marshaler = new MapFrame();
@@ -45,8 +46,37 @@ public class MapFrameTest {
    * Test method for {@link coyote.dataframe.marshal.MapFrame#marshal(coyote.dataframe.DataFrame)}.
    */
   @Test
-  public void testMarshalDataFrame()
-  {
-  }
+  public void testMarshalDataFrame() {
+    DataFrame frame = new DataFrame();
 
+    DataFrame vframe = new DataFrame();
+    vframe.add( "version", "1.7" );
+    DataFrame sframe = new DataFrame();
+    sframe.add( "specification", vframe );
+    frame.add( "java", sframe );
+
+    DataFrame nframe = new DataFrame();
+    nframe.add( "name", "alice" );
+    frame.add( "user", nframe );
+
+    MapFrame marshaler = new MapFrame();
+    Map map = marshaler.marshal( frame );
+    assertNotNull( map );
+    assertTrue( map.size() == 2 );
+    Object obj = map.get( "java" );
+    assertNotNull( obj );
+    assertTrue( obj instanceof Map );
+    Map jmap = (Map)obj;
+    assertTrue( jmap.size() == 1 );
+    obj = jmap.get( "specification" );
+    assertNotNull( obj );
+    assertTrue( obj instanceof Map );
+    Map smap = (Map)obj;
+    assertTrue( smap.size() == 1 );
+    obj = smap.get( "version" );
+    assertNotNull( obj );
+    assertTrue( obj instanceof String );
+    String ver = (String)obj;
+    assertEquals( "1.7", ver );
+  }
 }
